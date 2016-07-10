02696879b84b4cde1b1de55540fbcd90514f8add
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-07-10 12:32:34 +0200

    Reproduce Fable issue #245

--- src/extension.fsx
+++ src/extension.fsx
@@ -1,14 +1,23 @@
-#r "../node_modules/fable-import/Fable.Import.dll"
+#r "../node_modules/fable-core/Fable.Core.dll"
 #load "../node_modules/fable-import-vscode/Fable.Import.VSCode.fs"
  
 open Fable.Core
 open Fable.Import
 open Fable.Import.vscode
+    
+let pollingF () =
+  window.showInformationMessage "Hello from polling!"
+  |> ignore
 
-let activate (context : vscode.ExtensionContext) = 
-    printfn "Hello world"
-    
-    vscode.commands.Globals.registerCommand("extension.sayHello", fun _ ->
-        vscode.window.Globals.showInformationMessage "Hello world!" |> unbox )
-    |> context.subscriptions.Add
-    
+let helloWorld () =
+    let rec infiniteHello() =
+        async {
+            do! Async.Sleep 3000
+            pollingF()
+            return! infiniteHello()
+        }
+    infiniteHello() |> Async.StartAsPromise
+
+let activate (ctx: vscode.ExtensionContext) =
+    commands.registerCommand("extension.sayHello", fun _ -> helloWorld() |> unbox)
+    |> ctx.subscriptions.Add    
